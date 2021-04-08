a6eef22558fb22f619b58df73922d7eca98d0ef4
Keane Nguyen <kn2465@columbia.edu>
2021-04-08 01:41:16 -0400

    Baseline code for Policy and Evolution Strategy

--- evolution_strategy.py
+++ evolution_strategy.py
@@ -2,8 +2,11 @@ from gymenv_v2 import make_multiple_env
 import numpy as np
 from value import ValueFunction
 from policy import Policy   
-
 import wandb
+import torch
+from multiprocessing import Pool
+import copy
+            
 wandb.login()
 run=wandb.init(project="finalproject", entity="ieor-4575", tags=["training-easy"])
 #run=wandb.init(project="finalproject", entity="ieor-4575", tags=["training-hard"])
@@ -39,9 +42,10 @@ if __name__ == "__main__":
 
     # hyperparameters
     numtrajs = 1  # num of trajecories from the current policy to collect in each iteration
-    iterations = 1000  # total num of iterations
+    iterations = 100  # total num of iterations
     gamma = .99  # discount
     sigma = 0.01
+    N = 3
 
     # create env
     env = make_multiple_env(**easy_config) 
@@ -54,33 +58,67 @@ if __name__ == "__main__":
     
     # main iteration
     for ite in range(iterations): 
-        J = np.zeros(N,numtrajs)
-        delta = np.zeros(actor.shape)
+        weights = actor.get_weights() 
 
         for n in range(N):
             # Make a copy of the current network
+            epsilons = [np.random.random(layer.shape) if layer is not None else None for layer in weights]
             actor_clone = actor.clone()
-            weights = actor_clone.get_weights()
-            epsilons = random.random(shape = weights.shape)
-            actor_clone.update_weights(weights + sigma * epsilons)
-            J = 0
-            for num in range(numtrajs):
-                rews = actor_clone.rollout(env)
-                J+= np.sum([reward * gamma **i for i,reward in enumerate(rews)])
-            delta += J/numtrajs * epsilons / sigma
+            #assert actor_clone.model[0].weight.data.dtype == torch.float64
+            #assert all((list(actor_clone.model.parameters())[0] == list(actor.model.parameters())[0]).flatten()) # this is due to dtype when copying??
+
+            #TODO: This is really weird. Need to look into it.
+            #actor.model[0].weight.data is list(actor.model.parameters())[0].data
+            #actor.model[0].weight is list(actor.model.parameters())[0]
+
+            #TODO: Anothe weird thing, why does copy the network downgrade the dtype from  float64 down to float32?
+
+            new_weights = [layer + epsilons[i] * sigma if layer is not None else None for i,layer in enumerate(weights)]
+            actor_clone.update_weights(new_weights)
+            #assert all((list(actor_clone.model.parameters())[0] != list(actor.model.parameters())[0]).flatten())
+
+            #TODO: Multiprocessing output the exact same result ..
+            
+            # Use multiple processes to roll out faster
+            def rollout(combo):
+                actor, env, seed = combo
+                np.random.seed(seed)
+                return actor.rollout(env)
+
+            with Pool(5) as p:
+                results = (p.map(rollout,zip([copy.deepcopy(actor_clone) for _ in range(numtrajs)], [copy.deepcopy(env) for _ in range(numtrajs)], list(range(numtrajs)))))
+            
+            results, actions = list(zip(*results))
+            J = [round(np.sum([reward * gamma **i for i,reward in enumerate(result)]),3) for result in results]
+            print(f'iter = {ite}, n = {n}, J = {J}')
+            J = np.sum(J)
+
+            # J = 0
+            # actionss = []
+            # for num in range(numtrajs):
+            #     print(f'iter = {ite}, n = {n}, traj = {num}')
+            #     rews,actions = actor_clone.rollout(env)
+            #     actionss.append(actions)
+            #     J+= np.sum([reward * gamma **i for i,reward in enumerate(rews)])
+
+            # Update the weights variable which will later be used to update actor
+            weights = [layer + J/numtrajs * epsilons[i] / sigma/N if layer is not None else None for i,layer in enumerate(weights)] 
         
-        delta = delta/N
-        actor.update_weights(actor.weights + delta)
-        rews = actor.rollout()
-        rrecord.appenD(np.sum(rews))
+        test = list(actor.model.parameters())[0]
+        actor.update_weights(weights)
+        assert all((list(actor_clone.model.parameters())[0] != test).flatten())
+
+        rews, _ = actor.rollout(env)
+        rrecord.append(np.sum(rews))
 
         fixedWindow=100
         movingAverage=0
+        
         if len(rrecord) >= fixedWindow:
             movingAverage=np.mean(rrecord[len(rrecord)-fixedWindow:len(rrecord)-1])
         if ite % 50 == 0:
             print(f'Ite {ite}: movingAverage = {movingAverage}')
-        #wandb logging
+        
         wandb.log({ "training reward" : rrecord[-1], "training reward moving average" : movingAverage})
         
 
--- policy.py
+++ policy.py
@@ -1,252 +1,61 @@
 # define neural net \pi_\phi(s) as a class
+import torch
+import numpy as np
+import random
 
 class Policy(object):
     def __init__(self):
-        self.model =
+        pass
+        self.model = torch.nn.Sequential(
+                    torch.nn.Linear(10,16),
+                    torch.nn.ReLU(),
+                    torch.nn.Linear(16, 8),
+                    torch.nn.ReLU(),
+                    torch.nn.Linear(8, 1)
+                )
 
     def get_weights(self):
-        return self.model.parameters
+        return [layer.weight.data if hasattr(layer, 'weight') else None for layer in self.model ]
 
     def update_weights(self, new_weights):
-        for v,v_ in zip(new_weights, self.model.parameters()):
-            v_.data.copy_(v.data)
+        with torch.no_grad():
+            for i,layer in enumerate(self.model):
+                if hasattr(layer, 'weight'):
+                    layer.weight.data = new_weights[i]
+        # for v,v_ in zip(new_weights, self.model.parameters()):
+        #     v_.data.copy_(v.data)
         return None
 
     def clone(self):
         actor_clone = Policy()
         for v,v_ in zip(self.model.parameters(), actor_clone.model.parameters()):
-        v_.data.copy_(v.data)
+            v_.data.copy_(v.data)
         return actor_clone
 
-    def compute_prob(self):
+    def compute_prob(self, states, actions):
         """
         Compute probability for each action
         """
+        self.mnode
         pass
 
     def rollout(self, env):
         obs = env.reset()
         done = False
         rews =[]
+        i = 0
+        actions = []
         while not done:
+            if i%10 == 0:
+                print(f'Rolling out step: {i}')
             prob = self.compute_prob(np.expand_dims(obs,0))
             prob /= np.sum(prob)
             action = np.asscalar(np.random.choice(actsize, p=prob.flatten(), size=1)) #choose according distribution prob
-            
-            newobs, reward, done, info = env.step(list(action))
+            #action = np.random.randint(len(obs[-1].flatten()))
+            actions.append(action)
+            newobs, reward, done, info = env.step(action)
             rews.append(reward)
             obs = newobs
-        return rews
-
-class Policy(object):
-    
-    def __init__(self, policy_params):
-        
-        # TODO Define attention network 
-        self.numvars = policy_params['numvars']
-
-
-        self.model = torch.nn.Sequential(
-                    torch.nn.Linear(obssize,16),
-                    torch.nn.ReLU(),
-                    torch.nn.Linear(16, 8),
-                    torch.nn.ReLU(),
-                    torch.nn.Linear(8, actsize)
-                    #input layer of input size obssize
-                    #intermediate layers
-                    #output layer of output size actsize
-                )
-        
-        # DEFINE THE OPTIMIZER
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=lr)
-        
-        # RECORD HYPER-PARAMS
-        self.obssize = obssize
-        self.actsize = actsize
-        
-        # TEST
-        self.compute_prob(np.random.randn(obssize).reshape(1, -1))
-    
-    def compute_prob(self, states):
-        """
-        compute prob distribution over all actions given state: pi(s)
-        states: numpy array of size [numsamples, obssize]
-        return: numpy array of size [numsamples, actsize]
-        """
-        states = torch.FloatTensor(states)
-        prob = torch.nn.functional.softmax(self.model(states), dim=-1)
-        return prob.cpu().data.numpy()
-
-    def _to_one_hot(self, y, num_classes):
-        """
-        convert an integer vector y into one-hot representation
-        """
-        scatter_dim = len(y.size())
-        y_tensor = y.view(*y.size(), -1)
-        zeros = torch.zeros(*y.size(), num_classes, dtype=y.dtype)
-        return zeros.scatter(scatter_dim, y_tensor, 1)
-    
-    def train(self, states, actions, Qs):
-        """
-        states: numpy array (states)
-        actions: numpy array (actions)
-        Qs: numpy array (Q values)
-        """
-        #pdb.set_trace()
-        states = torch.FloatTensor(states)
-        actions = torch.LongTensor(actions)
-        Qs = torch.FloatTensor(Qs)
-        
-        # COMPUTE probability vector pi(s) for all s in states
-        logits = self.model(states)
-        prob = torch.nn.functional.softmax(logits, dim=-1)
-
-        # Compute probaility pi(s,a) for all s,a
-        action_onehot = self._to_one_hot(actions, actsize)
-        prob_selected = torch.sum(prob * action_onehot, axis=-1)
-        
-        # FOR ROBUSTNESS
-        prob_selected += 1e-8
-
-        # TODO define loss function as described in the text above
-#         print(Qs.shape)
-#         print(prob_selected.shape)
-        loss = - torch.mean(Qs * torch.log(prob_selected))
-
-        # BACKWARD PASS
-        self.optimizer.zero_grad()
-        loss.backward()
-
-        # UPDATE
-        self.optimizer.step()
-            
-        return loss.detach().cpu().data.numpy()
-
-
-class MLPRowFeatureAttenttionPolicy(Policy):
-
-    def __init__(self, policy_params):
-        Policy.__init__(self, policy_params)
-        self.numvars = policy_params['numvars']
- 
-        hsize = policy_params['hsize']
-        numlayers = policy_params['numlayers']
-        embeddeddim = policy_params['embed']
-        self.make_mlp_weights(policy_params['numvars']+1, embeddeddim, policy_params['hsize'], policy_params['numlayers'])
-
-        # build up filter
-        self.observation_filter = get_filter(policy_params['ob_filter'], shape = (self.numvars+1,))
-
-        # for visualization
-        """
-        self.baseobsdict = []
-        self.normalized_attentionmap = []
-        self.cutsdict = []
-        """
-        self.t = 0 
-
-    def make_mlp_weights(self, ob_dim, ac_dim, hsize, numlayers):
-        self.ob_dim = ob_dim
-        self.ac_dim = ac_dim
-        self.hsize = hsize
-        self.numlayers = numlayers
-        self.layers = []
-        self.offsets = [0]
-        for i in range(numlayers):
-            if i == 0:
-                layer = fclayer(nin=ob_dim, nout=hsize, act=tanh)
-                self.offsets.append(ob_dim * hsize + hsize)
-            else:
-                layer = fclayer(nin=hsize, nout=hsize, act=tanh)
-                self.offsets.append(hsize * hsize + hsize)
-            self.layers.append(layer)
-        finallayer = fclayer(nin=hsize, nout=ac_dim, act=lambda x:x, init_scale=0.01)
-        self.layers.append(finallayer)
-        self.offsets.append(hsize * ac_dim + ac_dim)
-        self.offsets = np.cumsum(self.offsets)
-
-    def act(self, ob, update=True, num=1):
-        t1 = time.time()
-        A,b,c0,cutsa,cutsb = ob
-        baseob_original = np.column_stack((A, b))
-        ob_original = np.column_stack((cutsa, cutsb))
-        
-        try:
-            totalob_original = np.row_stack((baseob_original, ob_original))
-        except:
-            print(baseob_original.shape, ob_original.shape)
-            print('no cut to add')
-            return []
-
-        # TODO: why do we need to normalize? 
-        # normalizatioon
-        totalob_original = (totalob_original - totalob_original.min()) / (totalob_original.max() - totalob_original.min())
-        totalob = self.observation_filter(totalob_original, update=update)
-
-        baseob = totalob[:A.shape[0],:]
-        ob = totalob[A.shape[0]:,:]
-
-        # base values - embed
-        x = baseob
-        for layer in self.layers:
-            #print(x.shape, layer.w.shape, layer.b.shape)
-            x = layer(x)        
-        baseembed = x
-
-        # cut values - embed
-        x = ob
-        for layer in self.layers:
-            #print(x.shape, layer.w.shape, layer.b.shape)
-            x = layer(x)
-        cutembed = x
-
-        # generate scores for each cut
-        attentionmap = cutembed.dot(baseembed.T)
-
-        score = np.mean(attentionmap, axis=-1)
-        assert score.size == ob.shape[0]
-
-        score -= np.max(score)
-
-        prob = np.exp(score) / np.sum(np.exp(score))
-
-        if np.ndim(prob) == 2:
-            assert np.shape(prob)[1] == 1
-            prob = prob.flatten()
-
-        if num == 1:
-            one_hot = np.random.multinomial(1,pvals=prob)
-            action = np.argmax(one_hot)
-        elif num >= 1:
-            # select argmax
-            num = np.min([num, prob.size])
-            threshold = np.sort(prob)[-num]
-            indices = np.where(prob >= threshold)
-            action = list(indices[0][:num])
-            assert len(action) == num
-
-        self.t += time.time() - t1
-        return action
-
-    def get_weights(self):
-        w = []
-        for layer in self.layers:
-            w.append(layer.get_weights())
-        return np.concatenate(w)
-
-    def update_weights(self, weights):
-        idx = 0
-        for i,j in zip(self.offsets[:-1],self.offsets[1:]):
-            params = weights[i:j]
-            self.layers[idx].update_weights(params)
-            idx += 1
-
-    def get_weights_plus_stats(self):     
-        mu, std = self.observation_filter.get_stats()
-        print('mu',mu,'std',std)
-        #aux = np.asarray([self.weights, mu, std])
-        #return aux
-        w = []
-        for layer in self.layers:
-            w.append(layer.get_weights())
-        return np.concatenate(w), mu, std
+            i += 1
+        print(f'Last rolling out step: {i}')
+        return rews, actions
